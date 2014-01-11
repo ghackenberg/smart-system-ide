54af34f6c189b792d2ff7137cb955d42cae75ee2
Georg Hackenberg <ghackenberg@gmail.com>
2014-01-11 12:19:31 +0000

    add min/avg/max objective to monitor

--- core/src/main/java/org/xtream/core/optimizer/monitors/CMDMonitor.java
+++ core/src/main/java/org/xtream/core/optimizer/monitors/CMDMonitor.java
@@ -1,6 +1,11 @@
 package org.xtream.core.optimizer.monitors;
 
+import java.util.List;
+import java.util.Map;
+
+import org.xtream.core.optimizer.Key;
 import org.xtream.core.optimizer.Monitor;
+import org.xtream.core.optimizer.State;
 
 public class CMDMonitor extends Monitor
 {
@@ -12,9 +17,9 @@ public class CMDMonitor extends Monitor
 	}
 
 	@Override
-	public void handle(int timepoint, int generatedStates, int validStates, int dominantStates, int equivalenceClasses)
+	public void handle(int timepoint, int generatedStates, int validStates, int dominantStates, double minObjective, double avgObjective, double maxObjective, Map<Key, List<State>> equivalenceClasses)
 	{	
-		System.out.println("Timepoint " + timepoint + " : " + generatedStates + " / " + validStates + " / " + dominantStates + " / " + equivalenceClasses + " / " + usedMemory() + " MB");
+		System.out.println("Timepoint " + timepoint + " : " + generatedStates + " / " + validStates + " / " + dominantStates + " / " + equivalenceClasses.size() + " / " + usedMemory() + " MB");
 	}
 
 	@Override
