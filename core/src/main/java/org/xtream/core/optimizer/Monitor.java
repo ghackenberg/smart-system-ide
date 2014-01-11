da88bffd14bba9b9bf0462e8a3f805afd2fd426a
Georg Hackenberg <ghackenberg@gmail.com>
2014-01-11 11:54:37 +0000

    provide memory information in monitors

--- core/src/main/java/org/xtream/core/optimizer/Monitor.java
+++ core/src/main/java/org/xtream/core/optimizer/Monitor.java
@@ -3,6 +3,26 @@ package org.xtream.core.optimizer;
 public abstract class Monitor
 {
 	
+	protected long totalMemory()
+	{
+		return Runtime.getRuntime().totalMemory() / 1024 / 1024;
+	}
+	
+	protected long freeMemory()
+	{
+		return Runtime.getRuntime().freeMemory() / 1024 / 1024;
+	}
+	
+	protected long maxMemory()
+	{
+		return Runtime.getRuntime().maxMemory() / 1024 / 1024;
+	}
+	
+	protected long usedMemory()
+	{
+		return totalMemory() - freeMemory();
+	}
+	
 	public abstract void start();
 	
 	public abstract void handle(int timepoint, int generatedStates, int validStates, int dominantStates, int equivalenceClasses);
