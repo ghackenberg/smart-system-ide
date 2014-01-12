4328091ed335dd802daa1b5c214b39ce35998a73
Georg Hackenberg <ghackenberg@gmail.com>
2014-01-12 12:36:15 +0000

    better readability of demo

--- demo/thermal/src/main/java/org/xtream/demo/thermal/model/RootComponent.java
+++ demo/thermal/src/main/java/org/xtream/demo/thermal/model/RootComponent.java
@@ -32,7 +32,7 @@ public class RootComponent extends Component
 	// COMPONENTS //
 	////////////////
 	
-	public NetComponent net = new NetComponent(new NetComponent(3), new NetComponent(3));
+	public NetComponent net = new NetComponent(new NetComponent(2), new NetComponent(2));
 	
 	//////////////
 	// CHANNELS //
