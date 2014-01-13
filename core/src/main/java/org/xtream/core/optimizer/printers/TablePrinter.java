b35783b786cc941f04fe228480d2f9b808aa6e43
Georg Hackenberg <ghackenberg@gmail.com>
2014-01-13 15:37:53 +0000

    automatically adjust column width

--- core/src/main/java/org/xtream/core/optimizer/printers/TablePrinter.java
+++ core/src/main/java/org/xtream/core/optimizer/printers/TablePrinter.java
@@ -4,6 +4,7 @@ import javax.swing.JScrollPane;
 import javax.swing.JTabbedPane;
 import javax.swing.JTable;
 import javax.swing.ScrollPaneConstants;
+import javax.swing.table.TableCellRenderer;
 
 import org.jdesktop.swingx.JXTreeTable;
 import org.jdesktop.swingx.treetable.DefaultTreeTableModel;
@@ -35,16 +36,30 @@ public class TablePrinter<T extends Component> extends Printer<T>
 		
 		table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
 		
-		table.getColumn(0).setPreferredWidth(120);
+		table.getColumn(0).setPreferredWidth(200);
 		table.getColumn(0).setHeaderValue("Tree");
 		
 		for (int i = 0; i < timepoint; i++)
 		{
-			table.getColumn(i + 1).setPreferredWidth(40);
+			table.getColumn(i + 1).setPreferredWidth(100);
 			table.getColumn(i + 1).setHeaderValue("" + i);
 		}
 		
 		table.expandAll();
+		
+		for (int column = 0; column < table.getColumnCount(); column++)
+		{
+			int width = 0;
+			
+			for (int row = 0; row < table.getRowCount(); row++)
+			{
+				TableCellRenderer renderer = table.getCellRenderer(row, column);
+				
+				width = Math.max(table.prepareRenderer(renderer, row, column).getPreferredSize().width, width);
+			}
+			
+			table.getColumn(column).setPreferredWidth(width);
+		}
 		
 		// Initialize scroll
 		
