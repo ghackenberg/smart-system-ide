ba6acefd831861b02735ded7a85104ab0d3f0f47
Georg Hackenberg <ghackenberg@gmail.com>
2014-01-17 10:33:10 +0000

    add standard costs aggregation component. improve graph viewer rendering.

--- demo/thermal/src/main/java/org/xtream/demo/thermal/model/nets/ModulesComponent.java
+++ demo/thermal/src/main/java/org/xtream/demo/thermal/model/nets/ModulesComponent.java
@@ -2,25 +2,40 @@ package org.xtream.demo.thermal.model.nets;
 
 import org.xtream.core.model.Port;
 import org.xtream.core.model.components.AbstractModulesComponent;
+import org.xtream.core.model.expressions.ChannelExpression;
+import org.xtream.demo.thermal.model.commons.EnergyModuleComponent;
 
-public abstract class ModulesComponent extends AbstractModulesComponent
+public class ModulesComponent extends AbstractModulesComponent
 {
 	
 	@SuppressWarnings("unchecked")
-	public ModulesComponent(int size)
+	public ModulesComponent(EnergyModuleComponent<?, ?, ?, ?, ?, ?>... modules)
 	{
+		super(modules);
+		
 		// Balance outputs
 		
-		balanceOutputs = new Port[size + 2];
-		
-		for (int i = 0; i < size + 2; i++)
+		balanceOutputs = new Port[modules.length];
+		for (int i = 0; i < modules.length; i++)
 		{
 			balanceOutputs[i] = new Port<>();
+		}
+		
+		// Balance channels
+		
+		balances = new ChannelExpression[modules.length];
+		for (int i = 0; i < modules.length; i++)
+		{
+			balances[i] = new ChannelExpression<>(balanceOutputs[i], modules[i].balanceOutput);
 		}
 	}
 	
 	// Ports
 	
 	public Port<Double>[] balanceOutputs;
+	
+	// Channels
+	
+	public ChannelExpression<Double>[] balances;
 	
 }
