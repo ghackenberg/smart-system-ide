dc7eb6ba604af7f5309cc2d64dc1aa49dcb9e530
Georg Hackenberg <ghackenberg@gmail.com>
2014-02-13 14:41:57 +0000

    überarbeitung der Ausgabe

--- demo/thermal/src/main/java/org/xtream/demo/thermal/model/objective/QuadraticStageBCalibrationRootComponent.java
+++ demo/thermal/src/main/java/org/xtream/demo/thermal/model/objective/QuadraticStageBCalibrationRootComponent.java
@@ -6,12 +6,27 @@ import org.xtream.demo.thermal.model.stage.StageB;
 public class QuadraticStageBCalibrationRootComponent extends LinearRootComponent
 {
 	
-	public static int STEPS = 5;
-	public static int ITERATIONS = 10;
+	public static int PROCESSORS = Runtime.getRuntime().availableProcessors() - 2;
+	
+	public static int DURATION = 96;
+	
+	public static int CLUSTERS_START = 10;
+	public static int CLUSTERS_END = 50;
+	public static int CLUSTERS_STEPS = 5;
+	
+	public static int SAMPLES_START = 10;
+	public static int SAMPLES_END = 50;
+	public static int SAMPLES_STEPS = 5;
+	
+	public static double RANDOM_START = 0.;
+	public static double RANDOM_END = 1.;
+	public static int RANDOM_STEPS = 5;
+	
+	public static int ITERATIONS = 20;
 	
 	public static void main(String[] args)
 	{
-		new Calibrator<>(QuadraticStageBCalibrationRootComponent.class).run(96, 1, 5, STEPS, 1, 5, STEPS, 0., 1., STEPS, ITERATIONS);
+		new Calibrator<>(QuadraticStageBCalibrationRootComponent.class).run(PROCESSORS, DURATION, CLUSTERS_START, CLUSTERS_END, CLUSTERS_STEPS, SAMPLES_START, SAMPLES_END, SAMPLES_STEPS, RANDOM_START, RANDOM_END, RANDOM_STEPS, ITERATIONS);
 	}
 
 	public QuadraticStageBCalibrationRootComponent()
