89f0f08efe253c8c52c9aa31877b8e9912a72e6d
Georg Hackenberg <ghackenberg@gmail.com>
2014-06-17 15:44:00 +0000

    minor fixes

--- core/src/main/java/org/xtream/core/workbench/parts/MonitorPart.java
+++ core/src/main/java/org/xtream/core/workbench/parts/MonitorPart.java
@@ -44,11 +44,11 @@ public class MonitorPart<T extends Component> extends Part<T>
 	{
 		super("Engine monitor", x, y, width, height);
 		
-		JFreeChart statesChart = ChartFactory.createLineChart("Number of generated, valid and dominant states", "Step", "States", states, PlotOrientation.VERTICAL, true, true, false);
-		JFreeChart classesChart = ChartFactory.createLineChart("Number of equivalence classes", "Step", "Clusters", classes, PlotOrientation.VERTICAL, true, true, false);
-		JFreeChart objectivesChart = ChartFactory.createLineChart("Mininum, average and maximum objective", "Step", "Costs", objectives, PlotOrientation.VERTICAL, true, true, false);
-		JFreeChart memoryChart = ChartFactory.createLineChart("Maximum, total and free memory", "Step", "Memory (in MB)", memory, PlotOrientation.VERTICAL, true, true, false);
-		JFreeChart timeChart = ChartFactory.createStackedAreaChart("Branch, norm, cluster, sort and stats time", "Step", "Time (in ms)", time, PlotOrientation.VERTICAL, true, true, false);
+		JFreeChart statesChart = ChartFactory.createLineChart("Number of generated, valid and dominant states", null, "States", states, PlotOrientation.VERTICAL, true, true, false);
+		JFreeChart classesChart = ChartFactory.createLineChart("Number of equivalence classes", null, "Clusters", classes, PlotOrientation.VERTICAL, true, true, false);
+		JFreeChart objectivesChart = ChartFactory.createLineChart("Mininum, average and maximum objective", null, "Costs", objectives, PlotOrientation.VERTICAL, true, true, false);
+		JFreeChart memoryChart = ChartFactory.createLineChart("Maximum, total and free memory", null, "Memory (in MB)", memory, PlotOrientation.VERTICAL, true, true, false);
+		JFreeChart timeChart = ChartFactory.createStackedAreaChart("Branch, norm, cluster, sort and stats time", null, "Time (in ms)", time, PlotOrientation.VERTICAL, true, true, false);
 		
 		statesChart.getCategoryPlot().getRenderer().setSeriesStroke(0, new BasicStroke(STROKE));
 		statesChart.getCategoryPlot().getRenderer().setSeriesStroke(1, new BasicStroke(STROKE));
@@ -82,24 +82,6 @@ public class MonitorPart<T extends Component> extends Part<T>
 		memoryChart.setTextAntiAlias(true);
 		timeChart.setTextAntiAlias(true);
 		
-		statesChart.getCategoryPlot().getDomainAxis().setLabel(null);
-		classesChart.getCategoryPlot().getDomainAxis().setLabel(null);
-		objectivesChart.getCategoryPlot().getDomainAxis().setLabel(null);
-		memoryChart.getCategoryPlot().getDomainAxis().setLabel(null);
-		timeChart.getCategoryPlot().getDomainAxis().setLabel(null);
-		
-		statesChart.getCategoryPlot().getDomainAxis().setTickMarksVisible(false);
-		classesChart.getCategoryPlot().getDomainAxis().setTickMarksVisible(false);
-		objectivesChart.getCategoryPlot().getDomainAxis().setTickMarksVisible(false);
-		memoryChart.getCategoryPlot().getDomainAxis().setTickMarksVisible(false);
-		timeChart.getCategoryPlot().getDomainAxis().setTickMarksVisible(false);
-		
-		statesChart.getCategoryPlot().getDomainAxis().setTickLabelsVisible(false);
-		classesChart.getCategoryPlot().getDomainAxis().setTickLabelsVisible(false);
-		objectivesChart.getCategoryPlot().getDomainAxis().setTickLabelsVisible(false);
-		memoryChart.getCategoryPlot().getDomainAxis().setTickLabelsVisible(false);
-		timeChart.getCategoryPlot().getDomainAxis().setTickLabelsVisible(false);
-		
 		statesChart.setPadding(new RectangleInsets(PADDING, PADDING, PADDING, PADDING));
 		classesChart.setPadding(new RectangleInsets(PADDING, PADDING, PADDING, PADDING));
 		objectivesChart.setPadding(new RectangleInsets(PADDING, PADDING, PADDING, PADDING));
@@ -113,8 +95,6 @@ public class MonitorPart<T extends Component> extends Part<T>
 		ChartPanel timePanel = new ChartPanel(timeChart);
 		
 		GridLayout layout = new GridLayout(5, 1);
-		layout.setHgap(1);
-		layout.setVgap(1);
 		
 		JPanel panel = new JPanel(layout);
 		panel.add(statesPanel);
@@ -139,25 +119,25 @@ public class MonitorPart<T extends Component> extends Part<T>
 	@Override
 	public void handle(int timepoint, Statistics statistics, Map<Key, List<State>> equivalenceClasses, State best)
 	{
-		states.addValue(statistics.generatedStates, "Generated states", "" + timepoint);
-		states.addValue(statistics.validStates, "Valid states", "" + timepoint);
-		states.addValue(statistics.dominantStates, "Dominant states", "" + timepoint);
+		states.addValue(statistics.generatedStates, "Generated", "" + timepoint);
+		states.addValue(statistics.validStates, "Valid", "" + timepoint);
+		states.addValue(statistics.dominantStates, "Preferred", "" + timepoint);
 		
-		classes.addValue(equivalenceClasses.size(), "Equivalence classes", "" + timepoint);
+		classes.addValue(equivalenceClasses.size(), "Count", "" + timepoint);
 		
-		objectives.addValue(statistics.minObjective, "Min objective", "" + timepoint);
-		objectives.addValue(statistics.avgObjective, "Avg objective", "" + timepoint);
-		objectives.addValue(statistics.maxObjective, "Max objective", "" + timepoint);
+		objectives.addValue(statistics.minObjective, "Minimum", "" + timepoint);
+		objectives.addValue(statistics.avgObjective, "Average", "" + timepoint);
+		objectives.addValue(statistics.maxObjective, "Maximum", "" + timepoint);
 		
-		memory.addValue(Memory.maxMemory(), "Max memory", "" + timepoint);
-		memory.addValue(Memory.totalMemory(), "Total memory", "" + timepoint);
-		memory.addValue(Memory.usedMemory(), "Used memory", "" + timepoint);
+		memory.addValue(Memory.maxMemory(), "Maximum", "" + timepoint);
+		memory.addValue(Memory.totalMemory(), "Total", "" + timepoint);
+		memory.addValue(Memory.usedMemory(), "Used", "" + timepoint);
 		
-		time.addValue(statistics.branch, "Branch time", "" + timepoint);
-		time.addValue(statistics.norm, "Norm time", "" + timepoint);
-		time.addValue(statistics.cluster, "Cluster time", "" + timepoint);
-		time.addValue(statistics.sort, "Sort time", "" + timepoint);
-		time.addValue(statistics.stats, "Stats time", "" + timepoint);
+		time.addValue(statistics.branch, "Branch", "" + timepoint);
+		time.addValue(statistics.norm, "Normalize", "" + timepoint);
+		time.addValue(statistics.cluster, "Cluster", "" + timepoint);
+		time.addValue(statistics.sort, "Sort", "" + timepoint);
+		time.addValue(statistics.stats, "Stats", "" + timepoint);
 	}
 
 }
