b4ae6a664bdbb90c33133a573286c390963fab13
Georg Hackenberg <ghackenberg@gmail.com>
2014-06-17 06:08:54 +0000

    add comments

--- core/src/main/java/org/xtream/core/optimizer/Strategy.java
+++ core/src/main/java/org/xtream/core/optimizer/Strategy.java
@@ -2,5 +2,5 @@ package org.xtream.core.optimizer;
 
 public abstract class Strategy
 {
-
+	// TODO define strategy interface
 }
--- core/src/main/java/org/xtream/core/optimizer/strategies/KMeans.java
+++ core/src/main/java/org/xtream/core/optimizer/strategies/KMeans.java
@@ -4,5 +4,5 @@ import org.xtream.core.optimizer.Strategy;
 
 public class KMeans extends Strategy
 {
-
+	// TODO implement k-means strategy as alternative to grid strategy
 }
