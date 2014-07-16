e574d49c7a07f897ad207c10e0be6d7e9944f2f0
Georg Hackenberg <ghackenberg@gmail.com>
2014-07-16 19:46:34 +0000

    add learning model

---
+++ demo/learning/src/main/java/org/xtream/demo/learning/model/ControlComponent.java
@@ -0,0 +1,25 @@
+package org.xtream.demo.learning.model;
+
+import org.xtream.core.model.Chart;
+import org.xtream.core.model.Expression;
+import org.xtream.core.model.Port;
+import org.xtream.core.model.charts.Histogram;
+import org.xtream.core.model.containers.Component;
+import org.xtream.core.model.expressions.ConstantNonDeterministicExpression;
+
+public class ControlComponent extends Component
+{
+	
+	// Ports
+	
+	public Port<Boolean> decisionOutput = new Port<>();
+	
+	// Charts
+	
+	public Chart decisionChart = new Histogram<>(decisionOutput);
+	
+	// Expressions
+	
+	public Expression<Boolean> decisionExpression = new ConstantNonDeterministicExpression<>(decisionOutput, true, false);
+
+}
---
+++ demo/learning/src/main/java/org/xtream/demo/learning/model/RootComponent.java
@@ -0,0 +1,104 @@
+package org.xtream.demo.learning.model;
+
+import org.xtream.core.model.Chart;
+import org.xtream.core.model.Port;
+import org.xtream.core.model.charts.Timeline;
+import org.xtream.core.model.containers.Component;
+import org.xtream.core.model.expressions.ChannelExpression;
+import org.xtream.core.workbench.Workbench;
+
+public class RootComponent extends Component
+{
+	
+	public static final int SIZE = 10;
+	public static final int DURATION = 100;
+	public static final int SAMPLES = 1000;
+	public static final int CLUSTERS = 1;
+	public static final double RANDOMNESS = 0;
+	public static final double CACHING = 0;
+	
+	public static void main(String[] args)
+	{
+		new Workbench<>(new RootComponent(SIZE), DURATION, SAMPLES, CLUSTERS, RANDOMNESS, CACHING);
+	}
+	
+	// Constructors
+	
+	@SuppressWarnings("unchecked")
+	public RootComponent(int size)
+	{
+		contexts = new ContextComponent[size];
+		
+		for (int i = 0; i < size; i++)
+		{
+			contexts[i] = new ContextComponent();
+		}
+		
+		controls = new ControlComponent[size];
+		
+		for (int i = 0; i < size; i++)
+		{
+			controls[i] = new ControlComponent();
+		}
+		
+		constraints = new ConstraintComponent[size];
+		
+		for (int i = 0; i < size; i++)
+		{
+			constraints[i] = new ConstraintComponent();
+		}
+		
+		objective = new ObjectiveComponent(size);
+		
+		decisions = new ChannelExpression[size];
+		
+		for (int i = 0; i < size; i++)
+		{
+			decisions[i] = new ChannelExpression<>(contexts[i].decisionInput, controls[i].decisionOutput);
+		}
+		
+		statesToConstraints = new ChannelExpression[size];
+		
+		for (int i = 0; i < size; i++)
+		{
+			statesToConstraints[i] = new ChannelExpression<>(constraints[i].stateInput, contexts[i].stateOutput);
+		}
+		
+		statesToObjective = new ChannelExpression[size];
+		
+		for (int i = 0; i < size; i++)
+		{
+			statesToObjective[i] = new ChannelExpression<>(objective.stateInputs[i], contexts[i].stateOutput);
+		}
+		
+		Port<Double>[] states = new Port[size];
+		
+		for (int i = 0; i < size; i++)
+		{
+			states[i] = contexts[i].stateOutput;
+		}
+		
+		statesChart = new Timeline(states);
+		
+		costChart = new Timeline(objective.costOutput);
+	}
+	
+	// Components
+	
+	public ContextComponent[] contexts;
+	public ControlComponent[] controls;
+	public ConstraintComponent[] constraints;
+	public ObjectiveComponent objective;
+	
+	// Channels
+	
+	public ChannelExpression<Boolean>[] decisions;
+	public ChannelExpression<Double>[] statesToConstraints;
+	public ChannelExpression<Double>[] statesToObjective;
+	
+	// Charts
+
+	public Chart statesChart;
+	public Chart costChart;
+
+}
