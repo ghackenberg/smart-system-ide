94382c87865e81559457b9880614b2b39c31b4a8
Georg Hackenberg <ghackenberg@gmail.com>
2014-09-12 16:56:09 +0000

    fix regression study (make it work!)

--- demo/hydro/src/main/java/org/xtream/demo/hydro/model/RegressionStudy.java
+++ demo/hydro/src/main/java/org/xtream/demo/hydro/model/RegressionStudy.java
@@ -2,6 +2,7 @@ package org.xtream.demo.hydro.model;
 
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.util.List;
 
 import org.apache.commons.math3.stat.regression.OLSMultipleLinearRegression;
@@ -11,163 +12,228 @@ import au.com.bytecode.opencsv.CSVReader;
 public class RegressionStudy
 {
 	
-	public static final int INFLOW_PAST = 20;
-	public static final int LEVEL_PAST = 20;
-	public static final int OUTFLOW_PAST = 20;
+	public static final int STAUSTUFE = 0;
+	
+	public static final int INFLOW_PAST = 10;
+	public static final int INFLOW_ORDER = 20;
+	
+	public static final int LEVEL_PAST = 3;
+	public static final int LEVEL_ORDER = 1;
+	
+	public static final int OUTFLOW_PAST = 10;
+	public static final int OUTFLOW_ORDER = 20;
+	
 	public static final int MAXIMUM_PAST = Math.max(LEVEL_PAST + 1, Math.max(INFLOW_PAST, OUTFLOW_PAST));
 
 	public static void main(String[] args)
 	{
 		try
-		{
-			System.out.println("Reading data ...");
+		{	
+			double[][] data_2011 = parseData("csv/Regression_2011.csv");
+			double[][] data_2012 = parseData("csv/Regression_2012.csv");
 			
-			CSVReader reader_2011 = new CSVReader(new FileReader("csv/Regression_2011.csv"), ';');
-			CSVReader reader_2012 = new CSVReader(new FileReader("csv/Regression_2012.csv"), ';');
+			double[] beta = trainModel(data_2012);
 			
-			List<String[]> lines_2011 = reader_2011.readAll();
-			List<String[]> lines_2012 = reader_2012.readAll();
-			
-			/*
-			for (int i = 0; i < lines_2011.size(); i++)
-			{
-				System.out.print("Row " + i + ": ");
-				
-				for (int j = 0; j < lines_2011.get(i).length; j++)
-				{
-					System.out.print((j > 0 ? ";" : "") + lines_2011.get(i)[j]);
-				}
-				
-				System.out.println();
-			}
-			*/
-			
-			double[][] data_2011 = new double[lines_2011.size() - 1][15];
-			double[][] data_2012 = new double[lines_2012.size() - 1][15];
-			
-			for (int i = 1; i < lines_2011.size(); i++)
-			{
-				for (int j = 1; j < 16; j++)
-				{
-					data_2011[i - 1][j - 1] = Double.parseDouble(lines_2011.get(i)[j].replace(',', '.'));
-				}
-			}
-			for (int i = 1; i < lines_2012.size(); i++)
-			{
-				for (int j = 1; j < 16; j++)
-				{
-					data_2012[i - 1][j - 1] = Double.parseDouble(lines_2012.get(i)[j].replace(',', '.'));
-				}
-			}
-			
-			reader_2011.close();
-			reader_2012.close();
-			
-			/*
-			for (int i = 0; i < data_2011.length; i++)
-			{
-				System.out.print("Row " + i + ": ");
-				
-				for (int j = 0; j < data_2011[i].length; j++)
-				{
-					System.out.print((j > 0 ? "," : "") + data_2011[i][j]);
-				}
-				
-				System.out.println();
-			}
-			*/
-			
-			System.out.println("Fitting model ...");
-			
-			OLSMultipleLinearRegression regression = new OLSMultipleLinearRegression();
-			
-			double[] y = new double[data_2011.length - MAXIMUM_PAST];
-			double[][] x = new double[data_2011.length - MAXIMUM_PAST][LEVEL_PAST + INFLOW_PAST + OUTFLOW_PAST];
-			
-			for (int i = 0; i < data_2011.length - MAXIMUM_PAST; i++)
-			{
-				y[i] = data_2011[i + MAXIMUM_PAST][1];
-				
-				for (int j = 0; j < LEVEL_PAST; j++)
-				{
-					x[i][j] = data_2011[i + MAXIMUM_PAST - 1 - j][1];
-				}
-				for (int j = 0; j < INFLOW_PAST; j++)
-				{
-					x[i][LEVEL_PAST + j] = data_2011[i + MAXIMUM_PAST - j][0];
-				}
-				for (int j = 0; j < OUTFLOW_PAST; j++)
-				{
-					x[i][LEVEL_PAST + INFLOW_PAST + j] = data_2011[i + MAXIMUM_PAST - j][2];
-				}
-			}
-			
-			/*
-			for (int i = 0; i < data_2011.length - MAXIMUM_PAST; i++)
-			{
-				System.out.print("Sample " + i + ": " + y[i] + " = ");
-
-				for (int j = 0; j < x[i].length; j++)
-				{
-					System.out.print((j > 0 ? " + " : "") + "b_" + j + " * " + x[i][j]);
-				}
-				
-				System.out.println();
-			}
-			*/
-			
-			regression.newSampleData(y, x);
-			
-			double[] beta = regression.estimateRegressionParameters();
-			
-			for (int i = 0; i < beta.length; i++)
-			{
-				System.out.println("beta_" + i + " = " + beta[i]);
-			}
-			
-			/*
-			double[] residuals = regression.estimateResiduals();
-			double[][] parametersVariance = regression.estimateRegressionParametersVariance();
-			double regressandVariance = regression.estimateRegressandVariance();
-			double rSquared = regression.calculateRSquared();
-			double sigma = regression.estimateRegressionStandardError();
-			*/
-			
-			System.out.println("Testing model ...");
-			
-			FileWriter result = new FileWriter("Regression.csv");
-			
-			result.write("Measured;Estimated\n");
-			
-			for (int i = MAXIMUM_PAST; i < data_2012.length; i++)
-			{
-				double y_measured = data_2012[i][1];
-				double y_estimated = 0;
-
-				for (int j = 0; j < LEVEL_PAST; j++)
-				{
-					y_estimated += beta[j] * data_2012[i - 1 - j][1];
-				}
-				for (int j = 0; j < INFLOW_PAST; j++)
-				{
-					y_estimated += beta[LEVEL_PAST + j] * data_2012[i - j][0];
-				}
-				for (int j = 0; j < OUTFLOW_PAST; j++)
-				{
-					y_estimated += beta[LEVEL_PAST + INFLOW_PAST + j] * data_2012[i - j][2];
-				}
-				
-				result.write(String.valueOf(y_measured).replace('.',',') + ";" + String.valueOf(y_estimated).replace('.',',') + "\n");
-			}
-			
-			result.close();
-			
-			System.out.println("Finish study ...");
+			testModel(beta, data_2011, "Comparison_2012_vs_2011.csv");
+			testModel(beta, data_2012, "Comparison_2012_vs_2012.csv");
 		}
 		catch (Exception e)
 		{
 			e.printStackTrace();
 		}
+	}
+	
+	private static double[][] parseData(String file) throws IOException
+	{
+		System.out.println("parseData(\"" + file + "\")");
+		
+		CSVReader reader = new CSVReader(new FileReader(file), ';');
+		
+		List<String[]> lines = reader.readAll();
+		
+		double[][] data = new double[lines.size() - 1][15];
+		
+		for (int i = 1; i < lines.size(); i++)
+		{
+			for (int j = 1; j < 16; j++)
+			{
+				data[i - 1][j - 1] = Double.parseDouble(lines.get(i)[j].replace(',', '.'));
+			}
+		}
+		
+		reader.close();
+		
+		/*
+		for (int i = 0; i < data_2011.length; i++)
+		{
+			System.out.print("Row " + i + ": ");
+			
+			for (int j = 0; j < data_2011[i].length; j++)
+			{
+				System.out.print((j > 0 ? "," : "") + data_2011[i][j]);
+			}
+			
+			System.out.println();
+		}
+		*/
+		
+		return data;
+	}
+	
+	private static double[] trainModel(double[][] data)
+	{
+		System.out.println("trainModel(data)");
+		
+		double[] y = new double[data.length - MAXIMUM_PAST];
+		double[][] x = new double[data.length - MAXIMUM_PAST][LEVEL_PAST * LEVEL_ORDER + INFLOW_PAST * INFLOW_ORDER + OUTFLOW_PAST * OUTFLOW_ORDER];
+		
+		for (int i = MAXIMUM_PAST; i < data.length; i++)
+		{
+			y[i - MAXIMUM_PAST] = data[i][STAUSTUFE * 3 + 1];
+			
+			for (int j = 0; j < LEVEL_PAST; j++)
+			{
+				for (int k = 0; k < LEVEL_ORDER; k++)
+				{
+					x[i - MAXIMUM_PAST][j * LEVEL_ORDER + k] = Math.pow(data[i - 1 - j][STAUSTUFE * 3 + 1], k + 1);
+				}
+			}
+			for (int j = 0; j < INFLOW_PAST; j++)
+			{
+				for (int k = 0; k < INFLOW_ORDER; k++)
+				{
+					x[i - MAXIMUM_PAST][LEVEL_PAST * LEVEL_ORDER + j * INFLOW_ORDER + k] = Math.pow(data[i - j][STAUSTUFE * 3 + 0], k + 1);
+				}
+			}
+			for (int j = 0; j < OUTFLOW_PAST; j++)
+			{
+				for (int k = 0; k < OUTFLOW_ORDER; k++)
+				{
+					x[i - MAXIMUM_PAST][LEVEL_PAST * LEVEL_ORDER + INFLOW_PAST * INFLOW_ORDER + j * OUTFLOW_ORDER + k] = Math.pow(data[i - j][STAUSTUFE * 3 + 2], k + 1);
+				}
+			}
+		}
+		
+		/*
+		for (int i = 0; i < data.length - MAXIMUM_PAST; i++)
+		{
+			System.out.print("Sample " + i + ": " + y[i] + " = ");
+
+			for (int j = 0; j < x[i].length; j++)
+			{
+				System.out.print((j > 0 ? " + " : "") + "b_" + j + " * " + x[i][j]);
+			}
+			
+			System.out.println();
+		}
+		*/
+		
+		OLSMultipleLinearRegression regression = new OLSMultipleLinearRegression();
+		
+		regression.newSampleData(y, x);
+		
+		double[] beta = regression.estimateRegressionParameters();
+		
+		/*
+		for (int i = 0; i < beta.length; i++)
+		{
+			System.out.println("beta_" + i + " = " + beta[i]);
+		}
+		
+		System.out.println(beta.length);
+		System.out.println(LEVEL_PAST * LEVEL_ORDER + INFLOW_PAST * INFLOW_ORDER + OUTFLOW_PAST * OUTFLOW_ORDER);
+		*/
+		
+		/*
+		double[] residuals = regression.estimateResiduals();
+		double[][] parametersVariance = regression.estimateRegressionParametersVariance();
+		double regressandVariance = regression.estimateRegressandVariance();
+		double rSquared = regression.calculateRSquared();
+		double sigma = regression.estimateRegressionStandardError();
+		*/
+		
+		return beta;
+	}
+	
+	private static double testModel(double[] beta, double[][] data, String file) throws IOException
+	{
+		System.out.println("testModel(beta, data, \"" + file + "\")");
+	
+		FileWriter result = new FileWriter(file);
+		
+		result.write("Measured;Estimated;Quotient\n");
+		
+		int count = 0;
+		double error = 0;
+		double[] y_estimated = new double[data.length];
+		
+		// Initialize estimated levels
+		
+		for (int i = 0; i < MAXIMUM_PAST; i++)
+		{
+			y_estimated[i] = data[i][STAUSTUFE * 3 + 1];
+		}
+		
+		for (int i = MAXIMUM_PAST; i < data.length; i++)
+		{	
+			double y_measured = data[i][STAUSTUFE * 3 + 1];
+			
+			// Stop as soon as missing data is found!
+			
+			if (y_measured == 0)
+			{
+				//System.out.println("Level measured zero: " + i);
+				
+				break;
+			}
+			
+			// Estimate level
+			
+			y_estimated[i] = beta[0];
+
+			for (int j = 0; j < LEVEL_PAST; j++)
+			{
+				for (int k = 0; k < LEVEL_ORDER; k++)
+				{
+					y_estimated[i] += beta[1 + j * LEVEL_ORDER + k] * Math.pow(y_estimated[i - 1 - j], k + 1);
+				}
+			}
+			for (int j = 0; j < INFLOW_PAST; j++)
+			{
+				for (int k = 0; k < INFLOW_ORDER; k++)
+				{
+					y_estimated[i] += beta[1 + LEVEL_PAST * LEVEL_ORDER + j * INFLOW_ORDER + k] * Math.pow(data[i - j][STAUSTUFE * 3 + 0], k + 1);
+				}
+			}
+			for (int j = 0; j < OUTFLOW_PAST; j++)
+			{
+				for (int k = 0; k < OUTFLOW_ORDER; k++)
+				{
+					y_estimated[i] += beta[1 + LEVEL_PAST * LEVEL_ORDER + INFLOW_PAST * INFLOW_ORDER + j * OUTFLOW_ORDER + k] * Math.pow(data[i - j][STAUSTUFE * 3 + 2], k + 1);
+				}
+			}
+			
+			if (y_estimated[i] == Double.NaN)
+			{
+				//System.out.println("Level estimated not a number: " + i);
+				
+				break;
+			}
+			
+			result.write(String.valueOf(y_measured).replace('.',',') + ";" + String.valueOf(y_estimated[i]).replace('.',',') + ";" + String.valueOf(y_estimated[i] / y_measured).replace('.',',') + "\n");
+			
+			count++;
+			
+			error += (y_measured - y_estimated[i]) * (y_measured - y_estimated[i]);
+		}
+		
+		error /= count;
+		
+		//System.out.println("Quadratic error: " + error);
+		
+		result.close();
+		
+		return error;
 	}
 
 }
