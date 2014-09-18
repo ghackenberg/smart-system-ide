f5dad4c6b575f4766fe58d4464e0a2999bb104da
Andreas Thut <andreas.thut@tum.de>
2014-09-18 20:22:31 +0000

    added constraint

--- demo/projecthouse/src/main/java/org/xtream/demo/projecthouse/model/thermalstorage/ThermalStorageContext.java
+++ demo/projecthouse/src/main/java/org/xtream/demo/projecthouse/model/thermalstorage/ThermalStorageContext.java
@@ -6,11 +6,13 @@ import org.xtream.core.model.Port;
 import org.xtream.core.model.State;
 import org.xtream.core.model.charts.Timeline;
 import org.xtream.core.model.containers.Module;
+import org.xtream.core.model.markers.Constraint;
 import org.xtream.demo.projecthouse.enums.OnOffDecision;
 import org.xtream.demo.projecthouse.model.room.RoomModule;
 
 public class ThermalStorageContext extends Module {
 	
+	protected static final double MIN_TEMP = 45.;
 	@SuppressWarnings("rawtypes")
 	public Port[] roomHeatingInputs;
 	public Port<Double> outerTemperatureInput = new Port<>();
@@ -20,6 +22,9 @@ public class ThermalStorageContext extends Module {
 	public Port<Integer> heatpumpInput = new Port<>();
 	
 	public Port<Double> temperatureOutput = new Port<>();
+	public Port<Boolean> temperatureValidOutput = new Port<>();
+	
+	public Constraint temperatureConstraint = new Constraint(temperatureValidOutput);
 	
 	public Chart temperature = new Timeline(temperatureOutput);
 	
@@ -36,12 +41,20 @@ public class ThermalStorageContext extends Module {
 		@Override
 		protected Double evaluate(State state, int timepoint) {
 			if(timepoint == 0) {
-				return 50.;
+				return MIN_TEMP;
 			}
 			double oldTemperature = temperatureOutput.get(state, timepoint - 1);
 			double outerTemperature = outerTemperatureInput.get(state, timepoint);
 			//TODO [Andreas]
 			return oldTemperature;
+		}
+	};
+	
+	public Expression<Boolean> temperatureValidExpression = new Expression<Boolean>(temperatureValidOutput) {
+
+		@Override
+		protected Boolean evaluate(State state, int timepoint) {
+			return temperatureOutput.get(state, timepoint) >= MIN_TEMP;
 		}
 	};
 	
