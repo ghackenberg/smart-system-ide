1cb98a0499133d019bee2fd15b7f85373163dae0
Georg Hackenberg <ghackenberg@gmail.com>
2014-09-22 11:56:31 +0000

    make basic 3d example work!

--- core/src/main/java/org/xtream/core/utilities/visitors/JoglLightVisitor.java
+++ core/src/main/java/org/xtream/core/utilities/visitors/JoglLightVisitor.java
@@ -4,6 +4,7 @@ import java.awt.Color;
 
 import javax.media.opengl.GL2;
 
+import org.apache.commons.math3.linear.RealMatrix;
 import org.apache.commons.math3.linear.RealVector;
 import org.xtream.core.model.Component;
 import org.xtream.core.model.State;
@@ -56,13 +57,31 @@ public class JoglLightVisitor extends Visitor
 	
 	public void handle(PointLightComponent light)
 	{
-		RealVector position = light.positionOutput.get(state, timepoint);
-		Color specular = light.specularOutput.get(state, timepoint);
-		Color diffuse = light.diffuseOutput.get(state, timepoint);
-		
-		gl2.glLightfv(GL2.GL_LIGHT0, GL2.GL_POSITION, new float[] {(float) position.getEntry(0), (float) position.getEntry(1), (float) position.getEntry(2), (float) position.getEntry(3)}, 0);
-		gl2.glLightfv(GL2.GL_LIGHT0, GL2.GL_SPECULAR, new float[] {specular.getRed() / 255.f, specular.getGreen() / 255.f, specular.getBlue() / 255.f, 1f}, 0);
-		gl2.glLightfv(GL2.GL_LIGHT0, GL2.GL_DIFFUSE, new float[] {diffuse.getRed() / 255.f, diffuse.getGreen() / 255.f, diffuse.getBlue() / 255.f, 1f}, 0);
+		gl2.glPushMatrix();
+		{
+			RealMatrix transform = light.transformInput.get(state, timepoint);
+			
+			double[] coefficients = new double[16];
+			
+			for (int col = 0; col < 4; col++)
+			{
+				for (int row = 0; row < 4; row++)
+				{
+					coefficients[col * 4 + row] = transform.getEntry(row, col);
+				}
+			}
+			
+			gl2.glMultMatrixd(coefficients, 0);
+			
+			RealVector position = light.positionOutput.get(state, timepoint);
+			Color specular = light.specularOutput.get(state, timepoint);
+			Color diffuse = light.diffuseOutput.get(state, timepoint);
+			
+			gl2.glLightfv(GL2.GL_LIGHT0, GL2.GL_POSITION, new float[] {(float) position.getEntry(0), (float) position.getEntry(1), (float) position.getEntry(2), (float) position.getEntry(3)}, 0);
+			gl2.glLightfv(GL2.GL_LIGHT0, GL2.GL_SPECULAR, new float[] {specular.getRed() / 255.f, specular.getGreen() / 255.f, specular.getBlue() / 255.f, 1f}, 0);
+			gl2.glLightfv(GL2.GL_LIGHT0, GL2.GL_DIFFUSE, new float[] {diffuse.getRed() / 255.f, diffuse.getGreen() / 255.f, diffuse.getBlue() / 255.f, 1f}, 0);
+		}
+		gl2.glPopMatrix();
 	}
 	
 	public void handle(DirectionalLightComponent light)
