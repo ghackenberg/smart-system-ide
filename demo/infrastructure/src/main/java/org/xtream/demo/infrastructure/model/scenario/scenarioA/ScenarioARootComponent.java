60fc8c6ccfbd5cd0b1fa0e417fd07709365e1979
Georg Hackenberg <ghackenberg@gmail.com>
2015-03-09 11:02:27 +0000

    delete generated files adapt to new engine API

--- demo/infrastructure/src/main/java/org/xtream/demo/infrastructure/model/scenario/scenarioA/ScenarioARootComponent.java
+++ demo/infrastructure/src/main/java/org/xtream/demo/infrastructure/model/scenario/scenarioA/ScenarioARootComponent.java
@@ -1,17 +1,19 @@
 package org.xtream.demo.infrastructure.model.scenario.scenarioA;
 
+import org.xtream.core.optimizer.beam.Engine;
+import org.xtream.core.optimizer.beam.strategies.GridStrategy;
 import org.xtream.core.workbench.Workbench;
 import org.xtream.core.workbench.parts.ComponentChartsPart;
 import org.xtream.core.workbench.parts.ComponentChildrenPart;
 import org.xtream.core.workbench.parts.ComponentHierarchyPart;
 import org.xtream.core.workbench.parts.ModelScenePart;
 import org.xtream.core.workbench.parts.StateSpacePart;
-import org.xtream.core.workbench.parts.charts.ClusterChartMonitorPart;
+import org.xtream.core.workbench.parts.charts.ClustersChartMonitorPart;
 import org.xtream.core.workbench.parts.charts.MemoryChartMonitorPart;
 import org.xtream.core.workbench.parts.charts.ObjectiveChartMonitorPart;
-import org.xtream.core.workbench.parts.charts.StateChartMonitorPart;
+import org.xtream.core.workbench.parts.charts.StatesChartMonitorPart;
 import org.xtream.core.workbench.parts.charts.TimeChartMonitorPart;
-import org.xtream.core.workbench.parts.charts.TraceChartMonitorPart;
+import org.xtream.core.workbench.parts.charts.TracesChartMonitorPart;
 import org.xtream.core.workbench.parts.charts.ViolationChartMonitorPart;
 import org.xtream.demo.infrastructure.model.RootComponent;
 import org.xtream.demo.infrastructure.model.scenario.Scenario;
@@ -25,7 +27,7 @@ public class ScenarioARootComponent extends RootComponent {
 	
 	public static <T> void main(String[] args)
 	{
-		new Workbench<>(new ScenarioARootComponent(), Scenario.DURATION, Scenario.SAMPLES, Scenario.CLUSTERS, Scenario.RANDOMNESS, Scenario.CACHING, Scenario.ROUNDS, true,  new ComponentHierarchyPart<>(0,0,1,2), new ComponentChildrenPart<>(0,2,1,2), new ViolationChartMonitorPart<>(3,0), new ModelScenePart<>(1,2,2,2), new ComponentChartsPart<>(3,2,2,2), new StateChartMonitorPart<>(5,0), new ClusterChartMonitorPart<>(5,1), new TraceChartMonitorPart<>(3,1), new StateSpacePart<>(3,1), new ObjectiveChartMonitorPart<>(4,1), new TimeChartMonitorPart<>(5,2), new MemoryChartMonitorPart<>(5,3));
+		new Workbench<>(new Engine<>(new ScenarioARootComponent(), Scenario.SAMPLES, Scenario.CLUSTERS, Scenario.BRANCH_ROUNDS, Scenario.BRANCH_DURATION, true, new GridStrategy()), Scenario.DURATION, new ComponentHierarchyPart<>(0,0,1,2), new ComponentChildrenPart<>(0,2,1,2), new ViolationChartMonitorPart<>(3,0), new ModelScenePart<>(1,2,2,2), new ComponentChartsPart<>(3,2,2,2), new StatesChartMonitorPart<>(5,0), new ClustersChartMonitorPart<>(5,1), new TracesChartMonitorPart<>(3,1), new StateSpacePart<>(3,1), new ObjectiveChartMonitorPart<>(4,1), new TimeChartMonitorPart<>(5,2), new MemoryChartMonitorPart<>(5,3));
 	}
 	
 }
